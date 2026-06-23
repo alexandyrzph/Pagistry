ff2292d0c6a28a0b4e7d0f04186052109019fcbc
Alexander <alexandyrzph@gmail.com>
2026-06-23 13:13:50 +0300

    ops: nightly pg_dump backup with 14-day retention

---
+++ ops/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+STAMP=$(date +%Y%m%d-%H%M%S)
+DEST=/home/ubuntu/pagecraft-backups
+mkdir -p "$DEST"
+docker compose -f /home/ubuntu/pagecraft/docker-compose.yml exec -T postgres \
+  pg_dump -U pagecraft pagecraft | gzip > "$DEST/pagecraft-$STAMP.sql.gz"
+# Keep the 14 most recent.
+ls -1t "$DEST"/pagecraft-*.sql.gz | tail -n +15 | xargs -r rm --
